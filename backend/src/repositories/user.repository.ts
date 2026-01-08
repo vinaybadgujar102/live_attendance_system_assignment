c64b6d1e675af626cadd8a01eb62fdeb501d4763
Vinay Badgujar <vinaybadgujar8@gmail.com>
2026-01-08 15:09:09 +0530

    Update user.repository.ts

--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -1,7 +1,39 @@
-export interface UserRepository {
-  create(user: User): Promise<User>;
-  findById(id: string): Promise<User | null>;
-  findByEmail(email: string): Promise<User | null>;
-  update(id: string, user: User): Promise<User>;
+import { User, type IUser } from "../models/User.model";
+
+export interface IUserRepository {
+  create(user: Partial<IUser>): Promise<IUser>;
+  findById(id: string): Promise<IUser | null>;
+  findByEmail(email: string): Promise<IUser | null>;
+  update(id: string, user: IUser): Promise<IUser | null>;
   delete(id: string): Promise<void>;
+}
+
+export class UserRepository implements IUserRepository {
+  async create(user: Partial<IUser>): Promise<IUser> {
+    const newUser = await User.create(user);
+    return newUser;
+  }
+
+  async findById(id: string): Promise<IUser | null> {
+    const user = await User.findById(id);
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<IUser | null> {
+    const user = await User.findOne({ email });
+    return user;
+  }
+
+  async update(id: string, user: IUser): Promise<IUser | null> {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { ...user },
+      { new: true, runValidators: true },
+    );
+    return updatedUser;
+  }
+
+  async delete(id: string): Promise<void> {
+    await User.findByIdAndDelete(id);
+  }
 }
