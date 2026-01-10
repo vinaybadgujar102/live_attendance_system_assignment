b01eb90e6f0d8f7406dea4932ac3403ef60585b6
Vinay Badgujar <vinaybadgujar8@gmail.com>
2026-01-10 11:19:47 +0530

    feat: created a response factory for both success and error responses to maintain consistency

---
+++ backend/utils/app.response.ts
@@ -0,0 +1,35 @@
+import type { Response } from "express";
+
+type ApiSuccessReponse<T> = {
+  success: true;
+  data: T;
+};
+
+type ApiErrorResponse = {
+  success: false;
+  error: string;
+};
+
+export const successResponse = <T extends Record<string, unknown>>(
+  res: Response,
+  statusCode: number,
+  data: T,
+) => {
+  const response: ApiSuccessReponse<T> = {
+    success: true,
+    data,
+  };
+  return res.status(statusCode).json(response);
+};
+
+export const errorResponse = (
+  res: Response,
+  statusCode: number,
+  errorMessage: string,
+) => {
+  const response: ApiErrorResponse = {
+    success: false,
+    error: errorMessage,
+  };
+  return res.status(statusCode).json(response);
+};
